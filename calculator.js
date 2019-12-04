1b496ae1b0a1477194f41c38157f13b748b755e2
Haley Brown <haleybrown@Haleys-MacBook-Air.local>
2019-12-04 06:57:27 -0800

    added BMI calculator.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 //jshint esversion:6
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/bmicalculator', function(req, res) {
+  res.sendFile(__dirname + '/bmicalculator.html');
+});
+
+app.post('/bmicalculator', function(req, res) {
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var BMI = weight/Math.pow(height, 2);
+  res.send('My BMI is ' + BMI + '.');
 });
 
 app.listen(port, function() {
